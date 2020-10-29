ca61fc8fedbce618219809e4bdc102a2976ef508
Tri <tri97nguyen@gmail.com>
2020-10-29 17:20:31 +0000

    change Twilio number

--- backend/services/twilio.js
+++ backend/services/twilio.js
@@ -1,6 +1,7 @@
 var accountSid = "AC90cb7dd6ead959ae5a48904d6606156d";
 var authToken = "b95f26b17082dfcefd64a545473d34da";
 
+
 var twilio = require('twilio');
 var express = require('express');
 
@@ -10,6 +11,6 @@ exports.sendSMS = (textMessage, toNumber) => {
     client.messages.create({
         body: textMessage,
         to: toNumber,  // Text this number
-        from: '+18605306660' // From a valid Twilio number
-    }).then((message) => console.log(message.sid));
+        from: '+18605306592' // From a valid Twilio number
+    }).then((message) => console.log(message.sid));  
 }
